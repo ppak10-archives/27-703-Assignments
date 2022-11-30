c83677eb64f0c44784fdad744e3ca810773d5191
Peter Pak <ppak10@gmail.com>
2022-11-30 12:58:54 -0500

    Plots inverse intercept lengths and determines alpha length.

---
+++ lab/instructional/Key_4new.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+ Created on Mon Apr 04 09:15:44 2020
+@author: Amaranth
+"""
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 21:58:44 2020
+
+@author: Amaranth
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 19:03:44 2020
+
+@author: Amaranth
+"""
+import time 
+import os
+import xlsxwriter
+path = 'D:/pp_img1'# Path for the images 
+import numpy as np
+import Lock_4new as h
+from datetime import datetime
+import matplotlib.pyplot as plt
+begin = time.time()
+row = 0
+column = 0
+files = os.listdir(path)
+abcd= [' ' for k in range (len(files)) ]
+Thick_table = np.zeros(len(files))
+mean_intercept = np.zeros(len(files))
+mean_inv_intercept=np.zeros(len(files))
+mean_in=np.zeros(len(files))
+frac=np.zeros(len(files))
+workbook = xlsxwriter.Workbook('Test.xlsx') 
+worksheet = workbook.add_worksheet()
+# distance in pixel on the scale bar
+distance_in_pixel=187   
+#distance mentioned on the scale bar in um
+distance_in_micrometer=10
+for i in range(len(files)):
+    dateTimeObj = datetime.now()
+    #Text file name for storing the variable details    
+    Variable_name='Ak_run_3' 
+#Type of adaptive thresholding to be applied. Choose from cv2.ADAPTIVE_THRESH_MEAN_C or cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+    Thresholding="cv2.ADAPTIVE_THRESH_MEAN_C"
+    
+#Angle step size to facilitate the degree of rotation.    
+    ang_size=180
+#Median Filter size to be used
+    median_filter=5
+#Adaptive thresholding filter size to be used
+    adpt_thresh_filter=11
+#Adaptive thresholding block constant to be subtracted
+    adpt_thresh_const=2.5
+#Denoise filter h, denoise template window size and denoise search window size 
+    DeNoise_h,DeNoise_template_window_size,DeNoise_search_window_size=2,3,3
+# kernal size to be used by morphological filters
+    kernal_a,kernal_b=3,3
+# Small object : Remove objects smaller than the specified size.
+    small_objects_min_size=2
+# Small holes : Remove contiguous holes smaller than the specified size.
+    small_holes_connectivity,small_holes_area_threshold=1,64 #64 is default
+    nlm,counts,bins,img1_blur,dst,c, d,distance, Inv_distance = h.measure_lath(path+'/'+files[i],ang_size) # number defines the angle of rotation
+    sum_distance=[]
+    sum_ind=[]
+    avg_beta=[]
+    sum_distance1=[]
+    sum_ind1=[]
+    
+    for j in range (len(distance)):
+        sum_distance1=sum_distance1+distance[j]
+    for k in range (len(Inv_distance)):
+        sum_ind1=sum_ind1+Inv_distance[k]   
+    sum_distance1=np.array(sum_distance1)	
+    
+
+    sum_ind1=np.array(sum_ind1)
+    for t in range (len(sum_ind1)):
+        if (sum_ind1[t] < 1):
+            sum_ind.append(sum_ind1[t])
+    for w in range (len(sum_distance1)):
+        if (sum_distance1[w] > 1):
+            sum_distance.append(sum_distance1[w])
+    sum_distance=np.array(sum_distance)	
+    sum_ind=np.array(sum_ind)   
+    avgdist=np.mean(sum_distance)
+    avgind=np.mean(sum_ind)
+    plt.figure(figsize=(10,10))
+    plt.title(str(files[i]))
+    plt.imshow(dst)
+    plt.savefig(str(files[i]),dpi= 300 ,format='Tif') #bbox_inches='tight',transparent=True, pad_inches=0
+    Thickness=0.67/avgind
+    Thick_um=(Thickness*distance_in_micrometer)/distance_in_pixel # the number in the denominator is the distance in pixels from imageJ (Analyse --> Set scale) while the number in numerator is the distance on scale (orginal image scale)
+    Thick_table[i] = Thick_um
+    mean_intercept[i]=((avgdist*distance_in_micrometer)/distance_in_pixel)#5-171;10-187(5000x)
+    #mean_inv_intercept[i]=((InvLamda*186)/10)
+    mean_in[i]=((avgind*distance_in_pixel)/distance_in_micrometer)
+    #abcd[i]=' '.join(files[i],Thick_table[i])
+    frac[i]=c
+    sum_distance=(sum_distance*distance_in_micrometer)/distance_in_pixel
+    sum_ind=(sum_ind*distance_in_pixel)/distance_in_micrometer
+    plt.figure(figsize=(10,10))
+    plt.title('Intercept_'+str(files[i]))
+    plt.hist(sum_distance, cumulative=True, bins = 100,density = True,  histtype='step',range= ((min(sum_distance)-0.5),(max(sum_distance)+0.5))) 
+    plt.xlabel('Intercepts (um)')
+    plt.ylabel('Cumulative Frequency')
+    plt.savefig('Intercept_'+str(files[i]),dpi= 300 , format='Tif')
+    
+    plt.figure(figsize=(10,10))
+    plt.title('Inverse Intercept_'+str(files[i]))
+    plt.hist(sum_ind,bins=100, cumulative=True , density=True, histtype='step',range= ((min(sum_ind)-0.5),(max(sum_ind)+0.5)))
+    plt.xlabel('Inverse Intercepts (1/um)')
+    plt.ylabel('Cumulative Frequency')
+    plt.savefig('Inverse Intercept_'+str(files[i]),dpi= 300 , format='Tif')
+	
+    abcd[i]=abcd.append(files[i]+'~'+str(mean_intercept[i])+'~'+str(mean_in[i])+'~'+str(frac[i])+'~'+str(Thick_table[i])+'~'+str(dateTimeObj))
+    print("The Thickness in um is :",Thick_um)
+    print("The file name is:",files[i])
+    print(dateTimeObj)
+	
+print(Thick_table)
+# Printing the information to the text file that will contain all the parameters.
+Variables = open("Variables_"+Variable_name+" .txt", "w")
+Variables.write("Distance in pixels - "+str(distance_in_pixel)+"\n"+"Distance in micrometer scale on SEM image - "+str(distance_in_micrometer)+"\n"+"Threshold_Type - "+Thresholding+"\n"+"Angle Step Size - "+str(ang_size)+"\n"+"Median Blur - "+str(median_filter)+ "\n"+"Adaptive threshold - "+str(adpt_thresh_filter)+","+str(adpt_thresh_const)+"\n"+"Kernal Size - "+"("+str(kernal_a)+","+str(kernal_b)+")"+"\n"+"Denoise - "+"("+str(DeNoise_h)+","+str(DeNoise_template_window_size)+","+str(DeNoise_search_window_size)+")"+"\n"+"Small Objects minimum size - "+str(small_objects_min_size)+"\n"+"Small Holes connectivity - "+str(small_holes_connectivity)+"\n"+"Small Holes area_threshold - "+str(small_holes_area_threshold))
+Variables.close()
+
+
+for i in abcd:
+    worksheet.write(row, column, i) 
+    row += 1
+workbook.close()
+time.sleep(1)
+end = time.time()
+print (f"Total runtime of the program is {end-begin}")
---
+++ lab/instructional/README.txt
@@ -0,0 +1,112 @@
+This README.txt file was generated on 20210209 by Venkata Satya Surya Amaranth Karra 
+
+-------------------
+GENERAL INFORMATION
+-------------------
+
+1. Title of Dataset: Quantification of Alpha in Ti-6Al-4V using OpenCV
+
+2. Author Information
+
+Corresponding / First Author Contact Information
+    Name: Venkata Satya Surya Amaranth Karra	
+    Institution: Carnegie Mellon University 	
+    Address: 5000 Forbes Avenue, Pittsburgh, Pennsylvania, USA, 15213
+    Email: vkarra@andrew.cmu.edu
+
+Author Contact Information
+    Name: Anthony D. Rollett
+    Institution: Carnegie Mellon University 	
+    Address: 5000 Forbes Avenue, Pittsburgh, Pennsylvania, USA, 15213
+    Email: rollett@andrew.cmu.edu
+    Office Phone Number: 412-268-3177
+
+Author Contact Information
+    Name: Amit K. Verma
+    Institution: Carnegie Mellon University 	
+    Address: 5000 Forbes Avenue, Pittsburgh, Pennsylvania, USA, 15213
+    Email: amitk2@andrew.cmu.edu	
+
+Author Contact Information
+    Name: Andrew Huck
+    Institution: Carnegie Mellon University 	
+    Address: 5000 Forbes Avenue, Pittsburgh, Pennsylvania, USA, 15213
+    Email: ahuck@andrew.cmu.edu	
+
+---------------------
+DATA & FILE OVERVIEW
+---------------------
+Directory of Files:
+   A. Filename:  Lock.py      
+      Short description: This python script contains the image processing operations and other
+operations like image rotation and calculation of the intercept length. This is written in the
+form of a function to be called by Key.py.       
+      
+   B. Filename:  Key.py     
+      Short description: This python script calls the Lock.py script and takes input for all the 
+parameters such as thresholding type, filter sizes of various image processing parameters, angle
+step size for rotation and image scale bar to calculate final lath thickness and give the output
+in excel sheet.        
+        
+   C. Filename:  Instructions.txt      
+      Short description: This is an basic instruction file on how to execute the python scripts and
+the process for viewing data and about the order in which the data appears. 
+
+
+Additional Notes on File Relationships, Context, or Content 
+(for example, if a user wants to reuse and/or cite your data, 
+what information would you want them to know?): N/A              
+
+File Naming Convention: N/A
+
+--------------------------
+METHODOLOGICAL INFORMATION
+--------------------------
+
+1. Software-specific information:
+
+Name: Spyder (Anaconda 3) IDE, Python 3.7 
+Version: 3.3.6 
+System Requirements: Windows 8 or newer, 64-bit macOS 10.13+, or Linux, including Ubuntu, RedHat, 
+CentOS 6+, and others. System architecture includes Windows- 64-bit x86, 32-bit x86; 
+MacOS- 64-bit x86; Linux- 64-bit x86, 64-bit Power8/Power9. Minimum 5 GB disk space to download
+and install. 
+Open Source? (Y/N): Y
+
+Name:  Microsoft Excel 
+Version: Microsoft 365 version (16) 64 bit
+System Requirements: Windows or macOS
+Open Source? (Y/N): N
+
+Additional Notes(such as, will this software not run on 
+certain operating systems?): N/A
+
+2. Equipment-specific information:
+
+Manufacturer: Dell 
+Model: G5 
+Additional Notes: Worked on Windows 10 laptop although, the code is designed to work on any device
+running python 3.0 with Spyder IDE
+
+3. Date of data collection (single date, range, approximate date): 20210221-20200130
+
+
+ Windows 10 laptop although, the code is designed to work on any device
+running python 3.0 with Spyder IDE
+
+3. Date of data collection (single date, range, approximate date): 20210221-20200130
+
+
+ 
+Additional Notes: Worked on Windows 10 laptop although, the code is designed to work on any device
+running python 3.0 with Spyder IDE
+
+3. Date of data collection (single date, range, approximate date): 20210221-20200130
+
+
+ Windows 10 laptop although, the code is designed to work on any device
+running python 3.0 with Spyder IDE
+
+3. Date of data collection (single date, range, approximate date): 20210221-20200130
+
+
